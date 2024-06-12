90ee567515eac4e41f2d93bcd56f5e465fb85b2d
Rwakasiisi Edwin <erwakasiisi@gmail.com>
2024-06-12 10:29:27 +0300

    more dictionary methods

--- rwakasiisi_edwin_day4.py
+++ rwakasiisi_edwin_day4.py
@@ -56,3 +56,30 @@ print(student_dict.get("technology_interest"))
 
 student_dict.update({"age": 25})
 print(student_dict)
+
+# Exercise 4: Use if to check if the age key is present in the dictionary
+if "age" in student_dict:
+    print("The age key is present in the dictionary")
+else:
+    print("The age key is not present in the dictionary")
+
+# Example 5:
+#Keys(), values() and items() method
+
+print(student_dict.keys())
+print(student_dict.values())
+print(student_dict.items())
+
+"""
+_summary_
+keys()      returns a view of objects that displays a list of all keys
+values()    returns a view of objects that displays a list of all values
+items()     returns a view of objects that displays a list of dictionary keys and values tuple pairs
+
+"""
+
+# Exercise 5: Use the update method to change course and add a new
+# key "WhatsApp_Number" to the dictionary
+
+student_dict.update({"course": "BSSE", "WhatsApp_Number": "08123456789"})
+print(student_dict)
