26031d1ced0b938537961081c1327b87d2737112
Rwakasiisi Edwin <erwakasiisi@gmail.com>
2024-06-12 09:33:14 +0300

    day 4

---
+++ rwakasiisi_edwin_day4.py
@@ -0,0 +1,25 @@
+# Dictionaries
+# Creating and using dictionaries
+# Dictionary methods and operations
+
+"""
+Dictionaries in python are collections of key and values
+-Unordered
+-Mutable
+-Indexed by keys
+
+"""
+# Example 1: Create a dictionary
+# Create a dictionary for a student persuing software engineering,
+# the key must be your name, age, technology interest, course and year of study.
+# Put your own details
+
+student_dict = {
+    "name": "Edwin",
+    "age": 23,
+    "technology_interest": "Data Science",
+    "course": "Software Engineering",
+    "year": 'Year 3'
+}
+
+print(student_dict["name"])
